42932ef2614636fdee907236fff8639149e2d3b6
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2019-11-17 16:19:42 -0800

    added new nodes, tests

--- test/basic.js
+++ test/basic.js
@@ -17,7 +17,12 @@ describe('parser -> codegen', () => {
     '(1 + 2)',
     '(1 + (200 * 31))',
     'Input(Bool())',
-    'val foo, _ = Bool()'
+    'val foo, _ = Bool()',
+    'var a = null',
+    'var a = true',
+    'var a = 100L',
+    'def a = 1.2',
+    'var a = 1F'
   ].map(a => {
     it(a, done => {
       const b = parseStat.call({}, a);
@@ -34,14 +39,36 @@ describe('nodes -> codegen', () => {
   const nodes = {
     '(5 + 7)': _.Term.ApplyInfix(
       _.Lit.Int(5), '+', [_.Lit.Int(7)]
+    ),
+    'var a = null': _.Defn.Var(
+      [_.Pat.Var('a')], _.Lit.Null()
+    ),
+    'val b = 42': _.Defn.Val(
+      [_.Pat.Var('b')], _.Lit.Int(42)
+    ),
+    'val c = true': _.Defn.Val(
+      [_.Pat.Var('c')], _.Lit.Boolean(true)
+    ),
+    'val c = 100L': _.Defn.Val(
+      [_.Pat.Var('c')], _.Lit.Long(100)
+    ),
+    'val d = "ABC"': _.Defn.Val(
+      [_.Pat.Var('d')], _.Lit.String('ABC')
+    ),
+    'var e = \'w\'': _.Defn.Var(
+      [_.Pat.Var('e')], _.Lit.Char('w')
+    ),
+    'var f, _ = Bool()': _.Defn.Var(
+      [_.Pat.Var('f'), _.Pat.Wildcard()], _.Term.Apply('Bool', [])
     )
   };
   Object.keys(nodes).map(key => {
     it(key, done => {
+      // expect(nodes[key]).to.deep.eq(parseStat.call({}, key));
       expect(generate(nodes[key])).to.eq(key);
       done();
-    })
-  })
+    });
+  });
 });
 
 /* eslint-env mocha */
